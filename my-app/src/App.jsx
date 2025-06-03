25712016d07c835d733993cde7461263db22fd75
Madhu <madhu0312shree@gmail.com>
2025-06-03 19:44:34 +0530

    Added

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,14 +1,38 @@
 // import { useState } from 'react'
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
- import './App.css'
-import React from 'react'
+//  import './App.css'
+import React ,{useState} from 'react'
 import axios from 'axios'
-
 function App() {
+  
+  const [movie,setMovie]=useState([])
+
+  const fetchMovie =()=>{
+    axios.get("https://www.omdbapi.com/?s=kabhi&apikey=57a3af3e")
+    .then((response)=>{
+
+      console.log(response);
+      setMovie(response.data.Search)
+    })
+  }
   return (
- <button>Fetch Movie</button>
+    <>
+
+ <button onClick={fetchMovie}>Fetch Movie</button>
+{
+  movie.map((value,index)=>{
+  return(
+      <h3 key={index}>{value.Title}</h3>
   );
+  })
+  
+ }
+
+ </>
+
+ 
+  )
 }
 
 export default App
