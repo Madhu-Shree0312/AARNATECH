0a339a429659c021f876a864d4163628647e6987
Madhu <madhu0312shree@gmail.com>
2025-06-04 16:59:01 +0530

    Added

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,41 +1,12 @@
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
- import './App.css'
-import React from 'react'
-// import axios from 'axios'
+import React from 'react';
+import './App.css';
 
-function App() {
-  return (
-   <>
- <nav className="navbar navbar-expand-lg bg-body-tertiary">
-  <div className="container-fluid">
-    <a className="navbar-brand" href="#">Navbar</a>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-        <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="#">Home</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="#">Link</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="#">kabhi</a>
-        </li>
-      </ul>
-      <form className="d-flex" role="search">
-        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-        <button className="btn btn-outline-success" type="submit">Search</button>
-      </form>
-    </div>
-  </div>
-</nav>
-   </>
+const App = () =>{
+  const url =" http://www.omdbapi.com/?apikey=57a3af3e&i=tt1285016";
+  
+  return(
+    <h1>This is our app component.</h1>
   )
+};
 
-}
-
-export default App
+export default App;
